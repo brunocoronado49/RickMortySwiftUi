14847844b6748d86fd4dc61b67fe319c8c10a2bf
brunocoronado49 <francorangelcoronado@gmail.com>
2024-05-28 21:03:01 -0600

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Bruno  on 28/05/24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
